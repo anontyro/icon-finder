1104b581f19a33091e1ffd7821773871721b7396
alexWilkinson <alex@hooq.tv>
2020-03-25 11:45:43 +0800

    split search bar into its own component

---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useCallback } from "react";
+import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import * as searchActions from "../../redux/modules/search/actions";
+import debounce from "lodash.debounce";
+
+const SearchContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 10px;
+  width: 100%;
+`;
+
+const SearchBox = styled.input`
+  padding: 10px;
+  width: 120px;
+  border-radius: 20px;
+  box-shadow: 3px 3px 0px 5px black;
+  transition: all 0.5s;
+  :focus {
+    width: 100%;
+  }
+`;
+
+interface SearchProps {}
+
+const Search: React.FC<SearchProps> = ({}) => {
+  const dispatch = useDispatch();
+  const [query, setQuery] = useState("");
+
+  const debounceSearch = useCallback(
+    debounce((val: string) => dispatch(searchActions.searchMovie(val)), 800),
+    []
+  );
+
+  return (
+    <SearchContainer>
+      <SearchBox
+        value={query}
+        onChange={e => {
+          e.persist();
+          setQuery(e.currentTarget.value);
+          debounceSearch(e.currentTarget.value);
+        }}
+      />
+    </SearchContainer>
+  );
+};
+
+export default Search;
